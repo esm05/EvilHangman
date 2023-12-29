32d30a9df977b9c5608684ca66bd478a40a8428e
emalia <emaliasnow05@gmail.com>
2023-12-29 13:29:15 -0500

    Removed unnecessary comments from lab 1 (work from lab 1)

--- Week2/main.c
+++ Week2/main.c
@@ -8,43 +8,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <crtdbg.h>
+//#include <crtdbg.h>
 #include "my_string.h"
 
 int main(int argc, char* argv[]) {
-	/*                   Lab 1					*/		
-	/*
-	printf("Hello World!");
-	printf("\tI have created a dynamically allocated 2D-array of 20 x 30 integers!\n");
-	// Dynamcially allocate a 2D array 30 X 20 
-	int columns = 30;
-	int rows = 20;
-
-	int** arr = malloc(rows * sizeof(int*));
-
-	for (int i = 0; i < rows; i++) {
-		arr[i] = (int*)malloc(columns * sizeof(int));
-	}
-
-	for (int i = 0; i < rows; i++) {
-		for (int j = 0; j < columns; j++) {
-			arr[i][j] = i + j;
-		}
-	}
-
-	for (int i = 0; i < rows; i++) {
-		for (int j = 0; j < columns; j++) {
-			printf("%2d ", arr[i][j]);
-		}
-		printf("\n");
-	}
-
-	//free memory
-	for (int i = 0; i < rows; i++) {
-		free(arr[i]);
-	}
-	free(arr);
-	*/
 	/*             Lab 2              */	
 	MY_STRING hMy_string = NULL;
 	//hMy_string = my_string_init_default();
@@ -61,6 +28,6 @@ int main(int argc, char* argv[]) {
 	my_string_destroy(&hMy_string);
 	my_string_destroy(&string1);
 	my_string_destroy(&string2);
-	_CrtDumpMemoryLeaks();
+	//_CrtDumpMemoryLeaks();
 	return 0;
 }
