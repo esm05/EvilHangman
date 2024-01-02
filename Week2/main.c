032dd89bce2d4c2b8b86adf3300c4c091a1df694
emalia <emaliasnow05@gmail.com>
2024-01-02 10:01:25 -0500

    Added statements in main function to test the functions created in this lab

--- Week2/main.c
+++ Week2/main.c
@@ -14,10 +14,22 @@
 int main(int argc, char* argv[]) {
 	/*             Lab 2              */	
 	MY_STRING hMy_string = NULL;
-	hMy_string = my_string_init_default();
-	//hMy_string = my_string_init_c_string("hi");
-	
-	
+        MY_STRING string1 = NULL;
+        MY_STRING string2 = NULL;
+        hMy_string = my_string_init_default();
+        //hMy_string = my_string_init_c_string("hi");
+
+        string1 = my_string_init_c_string("Spencer is amazing");
+        string2 = my_string_init_c_string("spencer");
+
+	printf("String 1 has a size: %d and capacity: %d\n", my_string_get_size(string1), my_string_get_capacity(string1));
+ 	printf("String 2 has a size: %d and capacity: %d\n", my_string_get_size(string2), my_string_get_capacity(string2));
+
+	printf("Compare string1 and string2: %d\n", my_string_compare(string1, string2));
+
 	my_string_destroy(&hMy_string);
+	my_string_destroy(&string1);
+        my_string_destroy(&string2);
+
 	return 0;
 }
